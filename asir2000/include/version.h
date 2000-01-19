d6e7edd047debdc0a389a4e5ee94d37bf2ca1ee5
noro <>
2000-01-19 02:13:05 +0000

    Updated the version number.

--- asir2000/include/version.h
+++ asir2000/include/version.h
@@ -1,2 +1,2 @@
-/* $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.4 1999/12/27 04:16:32 noro Exp $ */
-#define ASIR_VERSION 20000111
+/* $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.5 2000/01/11 06:43:36 noro Exp $ */
+#define ASIR_VERSION 20000119
