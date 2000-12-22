bb61a4e8d5dce26a45177e4f4dadb174dd08250d
saito <>
2000-12-22 10:03:29 +0000

    New file to interval number function.

---
+++ asir2000/builtin/itvnum.c
@@ -0,0 +1,479 @@
+/*
+ * $OpenXM: $
+ */
+
+#include "ca.h"
+#include "parse.h"
+#include "version.h"
+
+#if defined(INTERVAL)
+
+static void Pitv(NODE, Obj *);
+static void Pitvd(NODE, Obj *);
+static void Pitvbf(NODE, Obj *);
+static void Pinf(NODE, Obj *);
+static void Psup(NODE, Obj *);
+static void Pmid(NODE, Obj *);
+static void Pabsitv(NODE, Obj *);
+static void Pdisjitv(NODE, Obj *);
+static void Pinitv(NODE, Obj *);
+static void Pcup(NODE, Obj *);
+static void Pcap(NODE, Obj *);
+static void Pwidth(NODE, Obj *);
+static void Pdistance(NODE, Obj *);
+static void Pitvversion(Obj *);
+#endif
+static void Pprintmode(NODE, Obj *);
+
+#if	defined(__osf__) && 0
+int	end;
+#endif
+
+struct ftab interval_tab[] = {
+	{"printmode",Pprintmode,1},
+#if defined(INTERVAL)
+	{"itvd",Pitvd,-2},
+	{"intvald",Pitvd,-2},
+	{"itv",Pitv,-2},
+	{"intval",Pitv,-2},
+	{"itvbf",Pitvbf,-2},
+	{"intvalbf",Pitvbf,-2},
+	{"inf",Pinf,1},
+	{"sup",Psup,1},
+	{"absintval",Pabsitv,1},
+	{"disintval",Pdisjitv,2},
+	{"inintval",Pinitv,2},
+	{"cup",Pcup,2},
+	{"cap",Pcap,2},
+	{"mid",Pmid,1},
+	{"width",Pwidth,1},
+	{"diam",Pwidth,1},
+	{"distance",Pdistance,2},
+	{"iversion",Pitvversion,0},
+#endif
+	{0,0,0},
+};
+
+#if defined(INTERVAL)
+static void
+Pitvversion(Obj *rp)
+{
+	STOQ(ASIR_VERSION,(Q)*rp);
+}
+
+extern int	bigfloat;
+
+static void
+Pitv(NODE arg, Obj *rp)
+{
+	Num	a, i, s;
+	Itv	c;
+	double	inf, sup;
+
+#if 1
+	if ( bigfloat )
+		Pitvbf(arg, rp);
+	else
+		Pitvd(arg,rp);
+#else
+	asir_assert(ARG0(arg),O_N,"itv");
+	if ( argc(arg) > 1 ) {
+		asir_assert(ARG1(arg),O_N,"itv");
+		istoitv((Num)ARG0(arg),(Num)ARG1(arg),&c);
+	} else {
+		a = (Num)ARG0(arg);
+		if ( ! a ) {
+			*rp = 0;
+			return;
+		}
+		else if ( NID(a) == N_IP || NID(a) == N_IF) {
+			*rp = (Obj)a;
+			return;
+		}
+		else if ( NID(a) == N_ID ) {
+			inf = INF((ItvD)a);
+			sup = SUP((ItvD)a);
+			double2bf(inf, (BF *)&i);
+			double2bf(sup, (BF *)&s);
+			istoitv(i,s,&c);
+		}
+		else istoitv(a,a,&c);
+	}
+	if ( NID( c ) == N_IF ) addulp((ItvF)c, (ItvF *)rp);
+	else *rp = (Obj)c;
+#endif
+}
+
+static void
+Pitvbf(NODE arg, Obj *rp)
+{
+	Num	a, i, s;
+	Itv	c;
+	BF	ii,ss;
+	double	inf, sup;
+
+	asir_assert(ARG0(arg),O_N,"intvalbf");
+	a = (Num)ARG0(arg);
+	if ( argc(arg) > 1 ) {
+		asir_assert(ARG1(arg),O_N,"intvalbf");
+		i = (Num)ARG0(arg);
+		s = (Num)ARG1(arg);
+		ToBf(i, &ii);
+		ToBf(s, &ss);
+		istoitv((Num)ii,(Num)ss,&c);
+	} else {
+		if ( ! a ) {
+			*rp = 0;
+			return;
+		}
+		else if ( NID(a) == N_IP ) {
+			itvtois((Itv)a, &i, &s);
+			ToBf(i, &ii);
+			ToBf(s, &ss);
+			istoitv((Num)ii,(Num)ss,&c);
+		}
+		else if ( NID(a) == N_IF) {
+			*rp = (Obj)a;
+			return;
+		}
+		else if ( NID(a) == N_ID ) {
+			inf = INF((ItvD)a);
+			sup = SUP((ItvD)a);
+			double2bf(inf, (BF *)&i);
+			double2bf(sup, (BF *)&s);
+			istoitv(i,s,&c);
+		}
+		else {
+			ToBf(a, (BF *)&i);
+			istoitv(i,i,&c);
+		}
+	}
+	if ( c && OID( c ) == O_N && NID( c ) == N_IF ) addulp((ItvF)c, (ItvF *)rp);
+	else *rp = (Obj)c;
+}
+
+static void
+Pitvd(NODE arg, Obj *rp)
+{
+	double	inf, sup;
+	Num	a, a0, a1, t;
+	Itv	ia;
+	ItvD	d;
+
+	asir_assert(ARG0(arg),O_N,"intvald");
+	a0 = (Num)ARG0(arg);
+	if ( argc(arg) > 1 ) {
+		asir_assert(ARG1(arg),O_N,"intvald");
+		a1 = (Num)ARG1(arg);
+	} else {
+		if ( a0 && OID(a0)==O_N && NID(a0)==N_ID ) {
+			inf = INF((ItvD)a0);
+			sup = SUP((ItvD)a0);
+			MKItvD(inf,sup,d);
+			*rp = (Obj)d;
+			return;
+		}
+		a1 = (Num)ARG0(arg);
+	}
+	if ( compnum(0,a0,a1) > 0 ) {
+		t = a0; a0 = a1; a1 = t;
+	}
+	inf = ToRealDown(a0);
+	sup = ToRealUp(a1);
+	MKItvD(inf,sup,d);
+	*rp = (Obj)d;
+}
+
+static void
+Pinf(NODE arg, Obj *rp)
+{
+	Num	a, i, s;
+	Real	r;
+	double	d;
+
+	a = (Num)ARG0(arg);
+	if ( ! a ) {
+		*rp = 0;
+	} else if  ( OID(a) == O_N ) {
+		switch ( NID(a) ) {
+			case N_ID:
+				d = INF((ItvD)a);
+				MKReal(d, r);
+				*rp = (Obj)r;
+				break;
+			case N_IP:
+			case N_IF:
+			case N_IT:
+				itvtois((Itv)ARG0(arg),&i,&s);
+				*rp = (Obj)i;
+				break;
+			defaults:
+				*rp = (Obj)a;
+				break;
+		}
+	} else {
+		*rp = (Obj)a;
+	}
+}
+
+static void
+Psup(NODE arg, Obj *rp)
+{
+	Num	a, i, s;
+	Real	r;
+	double	d;
+
+	a = (Num)ARG0(arg);
+	if ( ! a ) {
+		*rp = 0;
+	} else if  ( OID(a) == O_N ) {
+		switch ( NID(a) ) {
+			case N_ID:
+				d = SUP((ItvD)a);
+				MKReal(d, r);
+				*rp = (Obj)r;
+				break;
+			case N_IP:
+			case N_IF:
+			case N_IT:
+				itvtois((Itv)ARG0(arg),&i,&s);
+				*rp = (Obj)s;
+				break;
+			defaults:
+				*rp = (Obj)a;
+				break;
+		}
+	} else {
+			*rp = (Obj)a;
+	}
+}
+
+static void
+Pmid(NODE arg, Obj *rp)
+{
+	Num	a, s;
+	Real	r;
+	double	d;
+
+	a = (Num)ARG0(arg);
+	if ( ! a ) {
+		*rp = 0;
+	} else switch (OID(a)) {
+		case O_N:
+			if ( NID(a) == N_ID ) {
+				d = ( INF((ItvD)a)+SUP((ItvD)a) ) / 2.0;
+				MKReal(d, r);
+				*rp = (Obj)r;
+			} else if ( NID(a) == N_IT ) {
+				error("mid: not supported operation");
+				*rp = 0;
+			} else if ( NID(a) == N_IP || NID(a) == N_IF ) {
+				miditvp((Itv)ARG0(arg),&s);
+				*rp = (Obj)s;
+			} else {
+				*rp = (Obj)a;
+			}
+			break;
+#if 0
+		case O_P:
+		case O_R:
+		case O_LIST:
+		case O_VECT:
+		case O_MAT:
+#endif
+		defaults:
+			*rp = (Obj)a;
+			break;
+	}
+}
+
+static void
+Pcup(NODE arg, Obj *rp)
+{
+	Itv	s;
+	Num	a, b;
+
+	asir_assert(ARG0(arg),O_N,"cup");
+	asir_assert(ARG1(arg),O_N,"cup");
+	a = (Num)ARG0(arg);
+	b = (Num)ARG1(arg);
+	if ( a && NID(a) == N_ID && b && NID(b) == N_ID ) {
+		cupitvd((ItvD)a, (ItvD)b, (ItvD *)rp);
+	} else {
+		cupitvp((Itv)ARG0(arg),(Itv)ARG1(arg),&s);
+		*rp = (Obj)s;
+	}
+}
+
+static void
+Pcap(NODE arg, Obj *rp)
+{
+	Itv	s;
+	Num	a, b;
+
+	asir_assert(ARG0(arg),O_N,"cap");
+	asir_assert(ARG1(arg),O_N,"cap");
+	a = (Num)ARG0(arg);
+	b = (Num)ARG1(arg);
+	if ( a && NID(a) == N_ID && b && NID(b) == N_ID ) {
+		capitvd((ItvD)a, (ItvD)b, (ItvD *)rp);
+	} else {
+		capitvp((Itv)ARG0(arg),(Itv)ARG1(arg),&s);
+		*rp = (Obj)s;
+	}
+}
+
+static void
+Pwidth(arg,rp)
+NODE arg;
+Obj *rp;
+{
+	Num	s;
+	Num	a;
+
+	asir_assert(ARG0(arg),O_N,"width");
+	a = (Num)ARG0(arg);
+	if ( ! a ) {
+		*rp = 0;
+	} else if ( NID(a) == N_ID ) {
+		widthitvd((ItvD)a, (Num *)rp);
+	} else {
+		widthitvp((Itv)ARG0(arg),&s);
+		*rp = (Obj)s;
+	}
+}
+
+static void
+Pabsitv(arg,rp)
+NODE arg;
+Obj *rp;
+{
+	Num	s;
+	Num	a, b;
+
+	asir_assert(ARG0(arg),O_N,"absitv");
+	a = (Num)ARG0(arg);
+	if ( ! a ) {
+		*rp = 0;
+	} else if ( NID(a) == N_ID ) {
+		absitvd((ItvD)a, (Num *)rp);
+	} else {
+		absitvp((Itv)ARG0(arg),&s);
+		*rp = (Obj)s;
+	}
+}
+
+static void
+Pdistance(arg,rp)
+NODE arg;
+Obj *rp;
+{
+	Num	s;
+	Num	a, b;
+
+	asir_assert(ARG0(arg),O_N,"distance");
+	asir_assert(ARG1(arg),O_N,"distance");
+	a = (Num)ARG0(arg);
+	b = (Num)ARG1(arg);
+	if ( a && NID(a) == N_ID && b && NID(b) == N_ID ) {
+		distanceitvd((ItvD)a, (ItvD)b, (Num *)rp);
+	} else {
+		distanceitvp((Itv)ARG0(arg),(Itv)ARG1(arg),&s);
+		*rp = (Obj)s;
+	}
+}
+
+static void
+Pinitv(arg,rp)
+NODE arg;
+Obj *rp;
+{
+	int	s;
+	Q	q;
+
+	asir_assert(ARG0(arg),O_N,"intval");
+	asir_assert(ARG1(arg),O_N,"intval");
+	if ( ! ARG1(arg) ) {
+		if ( ! ARG0(arg) ) s = 1;
+		else s = 0;
+	}
+	else if ( NID(ARG1(arg)) == N_ID ) {
+		s = initvd((Num)ARG0(arg),(ItvD)ARG1(arg));
+
+	} else if ( NID(ARG1(arg)) == N_IP || NID(ARG1(arg)) == N_IF ) {
+		if ( ! ARG0(arg) ) s = initvp((Num)ARG0(arg),(Itv)ARG1(arg));
+		else if ( NID(ARG0(arg)) == N_IP ) {
+			s = itvinitvp((Itv)ARG0(arg),(Itv)ARG1(arg));
+		} else {
+			s = initvp((Num)ARG0(arg),(Itv)ARG1(arg));
+		}
+	} else {
+		s = ! compnum(0,(Num)ARG0(arg),(Num)ARG1(arg));
+	}
+	STOQ(s,q);
+	*rp = (Obj)q;
+}
+
+static void
+Pdisjitv(arg,rp)
+NODE arg;
+Obj *rp;
+{
+	Itv	s;
+
+	asir_assert(ARG0(arg),O_N,"disjitv");
+	asir_assert(ARG1(arg),O_N,"disjitv");
+	error("disjitv: not implemented yet");
+	if ( ! s ) *rp = 0;
+	else *rp = (Obj)ONE;
+}
+
+#endif
+extern int	printmode;
+
+static void	pprintmode( void )
+{
+	switch (printmode) {
+#if defined(INTERVAL)
+		case MID_PRINTF_E:
+			fprintf(stderr,"Interval printing mode is a mitpoint type.\n");
+#endif
+		case PRINTF_E:
+			fprintf(stderr,"Printf's double printing mode is \"%%.16e\".\n");
+			break;
+#if defined(INTERVAL)
+		case MID_PRINTF_G:
+			fprintf(stderr,"Interval printing mode is a mitpoint type.\n");
+#endif
+		default:
+		case PRINTF_G:
+			fprintf(stderr,"Printf's double printing mode is \"%%g\".\n");
+			break;
+	}
+}
+
+static void
+Pprintmode(NODE arg, Obj *rp)
+{
+	int	l;
+	Q	a, r;
+
+	a = (Q)ARG0(arg);
+	if ( !a || NUM(a) && INT(a) ) {
+		l = QTOS(a);
+		if ( l < 0 ) l = 0;
+#if defined(INTERVAL)
+		else if ( l > MID_PRINTF_E ) l = 0;
+#else
+		else if ( l > PRINTF_E ) l = 0;
+#endif
+		STOQ(printmode,r);
+		*rp = (Obj)r;
+		printmode = l;
+		pprintmode();
+	} else {
+		*rp = 0;
+	}
+}
+	
+
---
+++ asir2000/engine/d-itv.c
@@ -0,0 +1,701 @@
+/*
+ * $OpenXM: $
+*/
+#if defined(INTERVAL)
+#include <float.h>
+#include "ca.h"
+#include "base.h"
+#if PARI
+#include "genpari.h"
+#endif
+
+#if defined(ITVDEBUG)
+void printbinint(int d)
+{
+	int	i, j, mask;
+	union {
+		int	x;
+		char	c[sizeof(int)];
+	} a;
+
+	a.x = d;
+	for(i=sizeof(int)-1;i>=0;i--) {
+		mask = 0x80;
+		for(j=0;j<8;j++) {
+			if (a.c[i] & mask) fprintf(stderr,"1");
+			else fprintf(stderr,"0");
+			mask >>= 1;
+		}
+	}
+	fprintf(stderr,"\n");
+}
+#endif
+
+double NatToRealUp(N a, int *expo)
+{
+	int *p;
+	int l,all,i,j,s,tb,top,tail;
+	unsigned int t,m[2];	
+	N	b,c;
+	int	kk, carry, rem;
+	
+	p = BD(a); l = PL(a) - 1;
+	for ( top = 0, t = p[l]; t; t >>= 1, top++ );
+	all = top + BSH*l; tail = (53-top)%BSH; j = l-(53-top)/BSH-1; 
+
+
+	if ( j >= 0 ) {
+
+#if defined(ITVDEBUG)
+	fprintf(stderr," %d : tail = %d\n", j, tail);
+	printbinint(p[j]);
+#endif
+		kk = (1<< (BSH - tail)) - 1;
+		rem = p[j] & kk;
+		if ( !rem )
+			for (i=0;i<j;i++)
+				if ( p[j] ) {
+					carry = 1;
+					break;
+				}
+		else carry = 1;
+		if ( carry ) {
+			b = NALLOC(j+1+1);
+			PL(b) = j+1;
+			p = BD(b);
+			for(i=0;i<j;i++) p[i] = 0;
+			p[j]=(1<< (BSH - tail));
+
+			addn(a,b,&c);
+
+			p = BD(c); l = PL(c) - 1;
+			for ( top = 0, t = p[l]; t; t >>= 1, top++ );
+			all = top + BSH*l; tail = (53-top)%BSH; i = l-(53-top)/BSH-1; 
+		} else i = j;
+	} else i = j;
+
+
+	m[1] = i < 0 ? 0 : p[i]>>(BSH-tail);
+	for ( j = 1, i++, tb = tail; i <= l; i++ ) {
+		s = 32-tb; t = i < 0 ? 0 : p[i];
+		if ( BSH > s ) {
+			m[j] |= ((t&((1<<s)-1))<<tb);
+			if ( !j ) 
+				break;
+			else {
+				j--; m[j] = t>>s; tb = BSH-s;
+			}
+		} else {
+			m[j] |= (t<<tb); tb += BSH;
+		}
+	}
+	s = (all-1)+1023;
+	m[0] = (m[0]&((1<<20)-1))|(MIN(2046,s)<<20); *expo = MAX(s-2046,0);
+#ifdef vax
+	t = m[0]; m[0] = m[1]; m[1] = t; itod(m);
+#endif
+#if defined(MIPSEL) || defined(TOWNS) || defined(linux) || defined(__alpha) || defined(__FreeBSD__) || defined(__NetBSD__)
+	t = m[0]; m[0] = m[1]; m[1] = t;
+#endif
+	return *((double *)m);
+}
+
+static double	Q2doubleDown(Q a)
+{
+	double nm,dn,t,snm,rd;
+	int enm,edn,e;
+	unsigned int *p,s;
+
+	nm = (SGN(a)>0)? NatToReal(NM(a),&enm): NatToRealUp(NM(a),&enm);
+
+	if ( INT(a) )
+		if ( enm >= 1 )
+			error("Q2doubleDown : Overflow");
+		else
+			return SGN(a)>0 ? nm : -nm;
+	else {
+		dn = (SGN(a)>0)? NatToReal(DN(a),&edn): NatToRealUp(DN(a),&edn);
+
+		if ( ((e = enm - edn) >= 1024) || (e <= -1023) )
+			error("Q2doubleDown : Overflow"); /* XXX */
+		else {
+			t = 0.0; p = (unsigned int *)&t; 	/* Machine */
+			*p = ((e+1023)<<20);
+#ifdef vax
+			s = p[0]; p[0] = p[1]; p[1] = s; itod(p);
+#endif
+#if defined(MIPSEL) || defined(TOWNS) || defined(linux) || defined(__alpha) || defined(__FreeBSD__) || defined(__NetBSD__)
+			s = p[0]; p[0] = p[1]; p[1] = s;
+#endif
+			FPMINUSINF
+			snm = (SGN(a)>0) ? nm : -nm;
+			rd  = snm / dn * t;
+			FPNEAREST
+			return rd;
+		}
+	}
+}
+
+
+static double	Q2doubleUp(Q a)
+{
+	double nm,dn,t,snm,rd;
+	int enm,edn,e;
+	unsigned int *p,s;
+
+	nm = (SGN(a)>0)? NatToRealUp(NM(a),&enm): NatToReal(NM(a),&enm);
+
+	if ( INT(a) )
+		if ( enm >= 1 )
+			error("Q2doubleUp : Overflow");
+		else
+			return SGN(a)>0 ? nm : -nm;
+	else {
+		dn = (SGN(a)>0)? NatToRealUp(DN(a),&edn): NatToReal(DN(a),&edn);
+
+		if ( ((e = enm - edn) >= 1024) || (e <= -1023) )
+			error("Q2doubleUp : Overflow"); /* XXX */
+		else {
+			t = 0.0; p = (unsigned int *)&t; 	/* Machine */
+			*p = ((e+1023)<<20);
+#ifdef vax
+			s = p[0]; p[0] = p[1]; p[1] = s; itod(p);
+#endif
+#if defined(MIPSEL) || defined(TOWNS) || defined(linux) || defined(__alpha) || defined(__FreeBSD__) || defined(__NetBSD__)
+			s = p[0]; p[0] = p[1]; p[1] = s;
+#endif
+#if 0
+			FPPLUSINF
+			snm = (SGN(a)>0) ? nm : -nm;
+			rd  = snm / dn * t;
+#endif
+
+			FPMINUSINF
+			snm = (SGN(a)>0) ? -nm : nm;
+			rd  = snm / dn * t;
+			FPNEAREST
+			return (-rd);
+		}
+	}
+}
+
+static double	PARI2doubleDown(BF a)
+{
+        GEN     p;
+	double  d;
+
+        ritopa(a, &p);
+        d = rtodbl(p);
+	cgiv(p);
+	return d;
+}
+
+static double	PARI2doubleUp(BF a)
+{
+	return PARI2doubleDown(a);
+}
+
+double	subulpd(double d)
+{
+	double inf;
+
+	FPMINUSINF
+	inf = d - DBL_MIN;
+	FPNEAREST
+	return inf;
+}
+
+double	addulpd(double d)
+{
+	double md, sup;
+
+	md = -d;
+	FPMINUSINF
+	sup = md - DBL_MIN;
+	FPNEAREST
+	return (-sup);
+}
+
+double	ToRealDown(Num a)
+{
+	double inf;
+
+	if ( ! a ) return 0.0;
+	switch ( NID(a) ) {
+		case N_Q:
+			inf = Q2doubleDown((Q)a); break;
+		case N_R:
+			inf = subulpd(BDY((Real)a)); break;
+		case N_B:
+			inf = PARI2doubleDown((BF)a); break;
+		case N_IP:
+			inf = ToRealDown(INF((Itv)a));
+			break;
+		case N_ID:
+			inf = INF((ItvD)a); break;
+		case N_A:
+		default:
+			inf = 0.0;
+			error("ToRealDown: not supported operands.");
+			break;
+	}
+	return inf;
+}
+
+double	ToRealUp(Num a)
+{
+	double sup;
+
+	if ( ! a ) return 0.0;
+	switch ( NID(a) ) {
+		case N_Q:
+			sup = Q2doubleUp((Q)a); break;
+		case N_R:
+			sup = addulpd(BDY((Real)a)); break;
+		case N_B:
+			sup = PARI2doubleUp((BF)a); break;
+		case N_IP:
+			sup = ToRealUp(SUP((Itv)a)); break;
+		case N_ID:
+			sup = SUP((ItvD)a); break;
+		case N_A:
+		default:
+			sup = 0.0;
+			error("ToRealUp: not supported operands.");
+			break;
+	}
+	return sup;
+}
+
+
+void	Num2double(Num a, double *inf, double *sup)
+{
+	switch ( NID(a) ) {
+		case N_Q:
+			*inf = Q2doubleDown((Q)a);
+			*sup = Q2doubleUp((Q)a);
+			break;
+		case N_R:
+			*inf = BDY((Real)a);
+			*sup = BDY((Real)a);
+			break;
+		case N_B:
+			*inf = PARI2doubleDown((BF)a);
+			*sup = PARI2doubleUp((BF)a);
+			break;
+		case N_IP:
+			*inf = ToRealDown(INF((Itv)a));
+			*sup = ToRealUp(SUP((Itv)a));
+			break;
+		case N_ID:
+			*inf = INF((ItvD)a);
+			*sup = SUP((ItvD)a);
+			break;
+		case N_A:
+		default:
+			*inf = 0.0;
+			*sup = 0.0;
+			error("Num2double: not supported operands.");
+			break;
+	}
+}
+
+
+void additvd(Num a, Num b, ItvD *c)
+{
+	double	ai,as,mas, bi,bs;
+	double	inf,sup;
+
+	if ( !a ) {
+		*c = (ItvD)b;
+	} else if ( !b ) {
+		*c = (ItvD)a;
+#if	0
+	} else if ( (NID(a) <= N_IP) && (NID(b) <= N_IP ) ) {
+		addnum(0,a,b,c);
+#endif
+	} else {
+
+		Num2double((Num)a,&ai,&as);
+		Num2double((Num)b,&bi,&bs);
+		mas = -as;
+		FPMINUSINF
+		inf = ai + bi;
+		sup = mas - bs;		/*  as + bs = ( - ( (-as) - bs ) ) */
+		FPNEAREST
+		MKItvD(inf,(-sup),*c);
+	}
+}
+
+void subitvd(Num a, Num b, ItvD *c)
+{
+	double	ai,as,mas, bi,bs;
+	double	inf,sup;
+
+	if ( !a ) {
+		*c = (ItvD)b;
+	} else if ( !b ) {
+		*c = (ItvD)a;
+#if	0
+	} else if ( (NID(a) <= N_IP) && (NID(b) <= N_IP ) ) {
+		addnum(0,a,b,c);
+#endif
+	} else {
+		Num2double(a,&ai,&as);
+		Num2double(b,&bi,&bs);
+		FPMINUSINF
+		inf = ai - bs;
+		sup = ( bi - as );	/* as - bi = ( - ( bi - as ) ) */
+		FPNEAREST
+		MKItvD(inf,(-sup),*c);
+	}
+}
+
+void	mulitvd(Num a, Num b, ItvD *c)
+{
+	double	ai,as,bi,bs,a1,a2,b1,b2,c1,c2,p;
+	double	inf, sup;
+	int	ba,bb;
+
+	if ( !a || !b ) {
+		*c = 0;
+#if	0
+	} else if ( (NID(a) <= N_IP) && (NID(b) <= N_IP ) ) {
+		mulnum(0,a,b,c);
+#endif
+	} else {
+		Num2double(a,&ai,&as);
+		Num2double(b,&bi,&bs);
+
+		FPMINUSINF
+
+		a2 = -as;
+		b2 = -bs;
+
+		if ( ba = ( a2 < 0.0 ) ) {
+			a1 = ai;
+		} else {
+			a1 = a2;
+			a2 = ai;
+		}
+		if ( bb = ( b2 < 0.0 ) ) {
+			b1 = bi;
+		} else {
+			b1 = b2;
+			b2 = bi;
+		}
+
+		c2 = - a2 * b2;
+		if ( b1 < 0.0 ) {
+			c1 = - a2 * b1;
+			if ( a1 < 0.0 ) {
+				p = - a1 * b2;
+				if ( p < c1 ) c1 = p;
+				p = - a1 * b1;
+				if ( p < c2 ) c2 = p;
+			}
+		} else {
+			c1 = (a1<0.0)?( - a1 * b2 ):( a1 * b1 );
+		}
+		if ( ba == bb ) {
+			inf = c1;
+			sup = - c2;
+		} else {
+			inf = c2;
+			sup = - c1;
+		}
+		FPNEAREST
+		MKItvD(inf,sup,*c);
+	}
+}
+
+int     initvd(Num a, ItvD b)
+{
+	Real inf, sup;
+
+	if ( NID(b) == N_ID ) {
+		MKReal(INF(b), inf);
+		MKReal(SUP(b), sup);
+	} else return 0;
+	if ( compnum(0,(Num)inf,a) > 0 ) return 0;
+	else if ( compnum(0,a,(Num)sup) > 0 ) return 0;
+	else return 1;
+}
+
+void	divitvd(Num a, Num b, ItvD *c)
+{
+	double	ai,as,bi,bs,a1,a2,b1,b2,c1,c2;
+	double	inf, sup;
+	int	ba,bb;
+
+	if ( !b ) {
+		*c = 0;
+		error("divitvd : division by 0");
+	} else if ( !a ) {
+		*c = 0;
+#if	0
+	} else if ( (NID(a) <= N_IP) && (NID(b) <= N_IP ) ) {
+		divnum(0,a,b,c);
+#endif
+	} else {
+		Num2double(a,&ai,&as);
+		Num2double(b,&bi,&bs);
+
+		FPMINUSINF
+
+		a2 = -as;
+		b2 = -bs;
+
+		if ( ba = ( a2 < 0.0 ) ) {
+			a1 = ai;
+		} else {
+			a1 = a2;
+			a2 = ai;
+		}
+		if ( bb = ( b2 < 0.0 ) ) {
+			b1 = bi;
+		} else {
+			b1 = b2;
+			b2 = bi;
+		}
+
+		if ( b1 <= 0.0 ) {
+			*c = 0;
+			error("divitvd : division by 0 interval");
+		} else {
+			c2 = a2 / b1;
+			c1 = (a1<0.0)?( a1 / b1 ):( - a1 / b2 );
+		}
+		if ( ba == bb ) {
+			inf = c1;
+			sup = - c2;
+		} else {
+			inf = c2;
+			sup = - c1;
+		}
+		FPNEAREST
+		MKItvD(inf,sup,*c);
+	}
+}
+
+void	pwritvd(Num a, Num e, ItvD *c)
+{
+	int	ei;
+	ItvD	t;
+
+	if ( !e ) {
+		*c = (ItvD)ONE;
+	}  else if ( !a ) {
+		*c = 0;
+#if	0
+	} else if ( NID(a) <= N_IP ) {
+		pwrnum(0,a,e,c);
+#endif
+	} else if ( !INT(e) ) {
+#if PARI && 0
+		GEN pa,pe,z;
+		int ltop,lbot;
+
+		ltop = avma; ritopa(a,&pa); ritopa(e,&pe); lbot = avma;
+		z = gerepile(ltop,lbot,gpui(pa,pe,prec));
+		patori(z,c); cgiv(z);
+#else
+		error("pwritvd : can't calculate a fractional power");
+#endif
+	} else {
+		ei = QTOS((Q)e);
+		pwritv0d((ItvD)a,ei,&t);
+		if ( SGN((Q)e) < 0 )
+			divnum(0,(Num)ONE,(Num)t,(Num *)c);
+		else
+			*c = t;
+	}
+}
+
+void	pwritv0d(ItvD a, int e, ItvD *c)
+{
+	double inf, sup;
+	double t, Xmin, Xmax;
+	int i;
+
+	if ( e == 1 )
+		*c = a;
+	else {
+		if ( !(e%2) ) {
+			if ( initvd(0,a) ) {
+				Xmin = 0.0;
+				t = - INF(a);
+				Xmax = SUP(a);
+				if ( t > Xmax ) Xmax = t;
+			} else {
+				if ( INF(a) > 0.0 ) {
+					Xmin = INF(a);
+					Xmax = SUP(a);
+				} else {
+					Xmin = - SUP(a);
+					Xmax = - INF(a);
+				}
+			}
+		} else {
+			Xmin = INF(a);
+			Xmax = SUP(a);
+		}
+		FPPLUSINF
+		sup = (Xmax!=0.0)?pwrreal0(Xmax,e):0.0;
+		FPMINUSINF
+		inf = (Xmin!=0.0)?pwrreal0(Xmin,e):0.0;
+		FPNEAREST
+		MKItvD(inf,sup,*c);
+	}
+}
+
+void	chsgnitvd(ItvD a, ItvD *c)
+{
+	double inf,sup;
+
+	if ( !a )
+		*c = 0;
+#if	0
+	else if ( NID(a) <= N_IP )
+		chsgnnum(a,c);
+#endif
+	else {
+		inf = - SUP(a);
+		sup = - INF(a);
+		MKItvD(inf,sup,*c);
+	}
+}
+
+int	cmpitvd(ItvD a, ItvD b)
+{
+	double	ai,as,bi,bs;
+
+	if ( !a ) {
+		if ( !b || (NID(b)<=N_IP) ) {
+			return compnum(0,(Num)a,(Num)b);
+		} else {
+			bi = INF(b);
+			bs = SUP(b);
+			if ( bi > 0.0 ) return -1;
+			else if ( bs < 0.0 ) return 1;
+			else return 0;
+		}
+	} else if ( !b ) {
+		if ( !a || (NID(a)<=N_IP) ) {
+			return compnum(0,(Num)a,(Num)b);
+		} else {
+			ai = INF(a);
+			as = SUP(a);
+			if ( ai > 0.0 ) return 1;
+			else if ( as < 0.0 ) return -1;
+			else return 0;
+		}
+	} else {
+		bi = INF(b);
+		bs = SUP(b);
+		ai = INF(a);
+		as = SUP(a);
+		if ( ai > bs ) return 1;
+		else if ( bi > as ) return -1;
+		else return 0;
+	}
+}
+
+void	miditvd(ItvD a, Num *b)
+{
+	double	t;
+	Real	rp;
+
+	if ( ! a ) *b = 0;
+	else if ( (NID(a) <= N_IP) )
+		*b = (Num)a;
+	else {
+		t = (INF(a) + SUP(a))/2.0;
+		MKReal(t,rp);
+		*b = (Num)rp;
+	}
+}
+
+void	cupitvd(ItvD a, ItvD b, ItvD *c)
+{
+	double	ai,as,bi,bs;
+	double	inf,sup;
+
+	ai = INF(a);
+	as = SUP(a);
+	bi = INF(b);
+	bs = SUP(b);
+	inf = MIN(ai,bi);
+	sup = MAX(as,bs);
+	MKItvD(inf,sup,*c);
+}
+
+void	capitvd(ItvD a, ItvD b, ItvD *c)
+{
+	double	ai,as,bi,bs;
+	double	inf,sup;
+
+	ai = INF(a);
+	as = SUP(a);
+	bi = INF(b);
+	bs = SUP(b);
+	inf = MAX(ai,bi);
+	sup = MIN(as,bs);
+	if ( inf > sup ) *c = 0;
+	else MKItvD(inf,sup,*c);
+}
+
+
+void	widthitvd(ItvD a, Num *b)
+{
+	double	t;
+	Real	rp;
+
+	if ( ! a ) *b = 0;
+	else {
+		t = SUP(a) - INF(a);
+		MKReal(t,rp);
+		*b = (Num)rp;
+	}
+}
+
+void	absitvd(ItvD a, Num *b)
+{
+	double	ai,as,bi,bs;
+	double	t;
+	Real	rp;
+
+	if ( ! a ) *b = 0;
+	else {
+		ai = INF(a);
+		as = SUP(a);
+		if (ai < 0) bi = -ai;	else	bi = ai;
+		if (as < 0) bs = -as;	else	bs = as;
+		if ( bi > bs )	t = bi; else	t = bs;
+		MKReal(t,rp);
+		*b = (Num)rp;
+	}
+}
+
+void	distanceitvd(ItvD a, ItvD b, Num *c)
+{
+	double	ai,as,bi,bs;
+	double	di,ds;
+	double	t;
+	Real	rp;
+
+	ai = INF(a);
+	as = SUP(a);
+	bi = INF(b);
+	bs = SUP(b);
+	di = bi - ai;
+	ds = bs - as;
+
+	if (di < 0) di = -di;
+	if (ds < 0) ds = -ds;
+	if (di > ds)	t = di; else	t = ds;
+	MKReal(t,rp);
+	*c = (Num)rp;
+}
+
+#endif
