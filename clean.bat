23e2374ecfc31e45d2a5cbc96d425b6dd3b0fddc
noro <>
2002-07-25 09:20:08 +0000

    A silly script for cleaning Windows related objects.

---
+++ clean.bat
@@ -0,0 +1,6 @@
+del /q asir2000\release\*.*
+del /q windows\asir2000lib\release\*.*
+del /q windows\engine2000\release\*.*
+del /q windows\asir32gui\release\*.*
+del /q windows\cpp\release\*.*
+del /q windows\help\*.chm
