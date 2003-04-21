d842d52087ee512683dca6f12877552c1dee21b0
noro <>
2003-04-21 09:18:29 +0000

    Updated makefiles.

--- windows/send-tgz.bat
+++ windows/send-tgz.bat
@@ -1,2 +1,2 @@
 scp ja/asirwin-ja.tgz en/asirwin-en.tgz orange:/home/web/Asir
-scp ja/asirwin-ja.tgz en/asirwin-en.tgz orange:/export/home01/ftp/pub/asir
+scp ja/asirwin-ja.tgz en/asirwin-en.tgz orange:/home/ftp/pub/asir
