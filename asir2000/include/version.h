4a2978d55da374d52b3af1f1d9d07d6251f42ae3
noro <>
2003-12-04 09:10:44 +0000

    Updated the version number.

--- asir2000/include/version.h
+++ asir2000/include/version.h
@@ -45,7 +45,7 @@
  * DEVELOPER SHALL HAVE NO LIABILITY IN CONNECTION WITH THE USE,
  * PERFORMANCE OR NON-PERFORMANCE OF THE SOFTWARE.
  *
- * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.141 2003/11/27 02:20:51 noro Exp $ 
+ * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.142 2003/12/02 03:24:24 noro Exp $ 
 */
-#define ASIR_VERSION 20031202
+#define ASIR_VERSION 20031204
 #define ASIR_DISTRIBUTION "Kobe"
