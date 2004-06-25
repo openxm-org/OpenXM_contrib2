8414a0885c30e4549d337901d9f701f87e978c98
noro <>
2004-06-25 05:21:56 +0000

    help() now displays a short description for module functions.

--- asir2000/include/version.h
+++ asir2000/include/version.h
@@ -45,7 +45,7 @@
  * DEVELOPER SHALL HAVE NO LIABILITY IN CONNECTION WITH THE USE,
  * PERFORMANCE OR NON-PERFORMANCE OF THE SOFTWARE.
  *
- * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.172 2004/06/21 00:58:32 noro Exp $ 
+ * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.173 2004/06/22 09:17:21 noro Exp $ 
 */
-#define ASIR_VERSION 20040622
+#define ASIR_VERSION 20040624
 #define ASIR_DISTRIBUTION "Kobe"
