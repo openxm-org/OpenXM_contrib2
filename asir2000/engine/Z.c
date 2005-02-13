2604903238c8302cefa1e376ec45c19b02cf46f3
fujiwara <>
2005-02-13 16:01:02 +0000

    change "inline" to "INLINE" macro for Windows.

--- asir2000/engine/Z.c
+++ asir2000/engine/Z.c
@@ -2,11 +2,19 @@
 #include "base.h"
 #include "inline.h"
 
-inline void _addz(Z n1,Z n2,Z nr);
-inline void _subz(Z n1,Z n2,Z nr);
-inline void _mulz(Z n1,Z n2,Z nr);
-inline int _addz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
-inline int _subz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
+#if defined(__GNUC__)
+#define INLINE inline
+#elif defined(VISUAL)
+#define INLINE __inline
+#else
+#define INLINE
+#endif
+
+INLINE void _addz(Z n1,Z n2,Z nr);
+INLINE void _subz(Z n1,Z n2,Z nr);
+INLINE void _mulz(Z n1,Z n2,Z nr);
+INLINE int _addz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
+INLINE int _subz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
 
 /* immediate int -> Z */
 #define UTOZ(c,n) (n)=(!((unsigned int)(c))?0:(((unsigned int)(c))<=IMM_MAX?((Z)((((unsigned int)(c))<<1)|1)):utoz((unsigned int)(c))))
