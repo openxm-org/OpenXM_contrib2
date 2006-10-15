53b1324978d6d19d58cd45e2b0ac112884ad7cd3
noro <>
2006-10-15 23:44:30 +0000

    Removed INLINE declarations.

--- asir2000/engine/Z.c
+++ asir2000/engine/Z.c
@@ -13,8 +13,8 @@
 INLINE void _addz(Z n1,Z n2,Z nr);
 INLINE void _subz(Z n1,Z n2,Z nr);
 INLINE void _mulz(Z n1,Z n2,Z nr);
-INLINE int _addz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
-INLINE int _subz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
+int _addz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
+int _subz_main(unsigned int *m1,int d1,unsigned int *m2,int d2,unsigned int *mr);
 
 /* immediate int -> Z */
 #define UTOZ(c,n) (n)=(!((unsigned int)(c))?0:(((unsigned int)(c))<=IMM_MAX?((Z)((((unsigned int)(c))<<1)|1)):utoz((unsigned int)(c))))
