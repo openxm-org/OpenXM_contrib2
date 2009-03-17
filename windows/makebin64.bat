c464bc5ece1e8a8c83289f6504dd31d8e2833870
ohara <>
2009-03-17 13:38:22 +0000

    Risa/Asir for Windows x64 uses the pari library if the environment variable WITH_PARI does not be set to no.

--- windows/makebin64.bat
+++ windows/makebin64.bat
@@ -1,12 +1,18 @@
-@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.1 2009/03/09 16:00:42 ohara Exp $
+@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.2 2009/03/10 16:26:12 ohara Exp $
 @echo off
 
 set ARCH=AMD64
-set WITH_PARI=no
+if not defined WITH_PARI set WITH_PARI=yes
 
 cd gc-7.1
 nmake -f NT_X64_STATIC_THREADS_MAKEFILE gc.lib
 cd ..
+
+if "%WITH_PARI%" == "yes" (
+  cd pari20
+  nmake -f Makefile.vc6
+  cd ..
+)
 
 for %%i in ( asir2000lib engine2000 cpp asir32gui ) do (
   cd %%i
