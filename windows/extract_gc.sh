b3bd63c952f0f6f179e68a3f43aa5428357e5996
ohara <>
2009-11-24 05:00:23 +0000

    A pari source can be extracted by this script.

--- windows/extract_gc.sh
+++ windows/extract_gc.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-# $OpenXM$
+# $OpenXM: OpenXM_contrib2/windows/extract_gc.sh,v 1.2 2009/03/11 11:32:07 ohara Exp $
 (shopt -s igncr) 2>/dev/null && eval 'shopt -s igncr';#
 OpenXM_HOME=${OpenXM_HOME:-../../OpenXM}
 OpenXM_dist=${OpenXM_HOME}/../OpenXM_dist
 GC=gc-7.1
 GC_TARGZ=${GC}.tar.gz
 GC_PATCH=gc-7.0-risa.diff
+PARI_TARGZ=pari-2.0.17.beta.tgz
 
+cat ${OpenXM_dist}/${PARI_TARGZ} | ( cd pari20; tar zxf -)
 tar zxf ${OpenXM_dist}/${GC_TARGZ}
 
 for i in ${GC_PATCH} ; do 
