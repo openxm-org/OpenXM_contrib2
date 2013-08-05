49db1754314adbc65f812cf77ca683d320bfc88d
ohara <>
2013-08-05 11:41:40 +0000

    The script terminates if gc-7.2 does not exist.

--- windows/makebin6.bat
+++ windows/makebin6.bat
@@ -1,16 +1,16 @@
-@rem $OpenXM$
+@rem $OpenXM: OpenXM_contrib2/windows/makebin6.bat,v 1.1 2009/03/09 09:33:30 ohara Exp $
 @echo off
 
 for %%i in ( pari20 asir2000lib engine2000 cpp asir32gui ) do (
-  cd %%i
+  pushd %%i
   nmake -f Makefile.vc6
-  cd ..
+  popd
 )
 
-cd asir32gui
+pushd asir32gui
 nmake -f Makefile.vc6 LANG=en
-cd ..
+popd
 
-cd ..\asir2000
+pushd ..\asir2000
 nmake -f Makefile.vc6
-cd ..\windows
+popd
--- windows/makebin64.bat
+++ windows/makebin64.bat
@@ -1,26 +1,27 @@
-@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.7 2012/05/09 06:17:13 ohara Exp $
+@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.8 2012/05/30 02:24:33 ohara Exp $
 @echo off
 
 set ARCH=AMD64
 
-cd gc-7.2
+if not exist gc-7.2 ( echo run ./extract-gc.sh in cyngwin & exit /b 1 )
+pushd gc-7.2
 nmake -f NT_X64_STATIC_THREADS_MAKEFILE gc.lib
-cd ..
+popd
 
-cd pari20
+pushd pari20
 nmake -f Makefile.vc6
-cd ..
+popd
 
 for %%i in ( asir2000lib engine2000 mcpp asir32gui ) do (
-  cd %%i
+  pushd %%i
   nmake -f Makefile.vc
-  cd ..
+  popd
 )
 
-cd asir32gui
+pushd asir32gui
 nmake -f Makefile.vc LANG=en
-cd ..
+popd
 
-cd ..\asir2000
+pushd ..\asir2000
 nmake -f Makefile.vc
-cd ..\windows
+popd
