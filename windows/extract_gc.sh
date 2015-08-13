1d94a4937c58757f65205973fc09f42a99acb639
ohara <>
2015-08-13 13:45:26 +0000

    Removed pari.

--- windows/extract_gc.sh
+++ windows/extract_gc.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# $OpenXM: OpenXM_contrib2/windows/extract_gc.sh,v 1.6 2012/05/30 02:24:33 ohara Exp $
+# $OpenXM: OpenXM_contrib2/windows/extract_gc.sh,v 1.7 2013/08/20 14:17:22 ohara Exp $
 (set -o igncr) 2>/dev/null && set -o igncr;#
 OpenXM_HOME=${OpenXM_HOME:-../../OpenXM}
 OpenXM_dist="$(realpath -m ${OpenXM_HOME}/../OpenXM_dist)"
-GC=gc-7.2
-GC_TARGZ=gc-7.2b.tar.gz
+GC=gc-7.4.2
+ATOMIC_OPS=libatomic_ops-7.4.0
+GC_TARGZ=gc-7.4.2.tar.gz
 GC_PATCH=gc-7.0-risa.diff
-PARI_TARGZ=pari-2.0.17.beta.tgz
 
-cat ${OpenXM_dist}/${PARI_TARGZ} | ( cd pari20; tar zxf -)
 tar zxf ${OpenXM_dist}/${GC_TARGZ}
+tar zxf ${OpenXM_dist}/${ATOMIC_OPS}.tar.gz
+mv ${GC} gc
+mv ${ATOMIC_OPS} gc/libatomic_ops
 
 for i in ${GC_PATCH} ; do 
 	f=../asir2000/$i
 	if [ -f $f ]; then
-		(cd ${GC}; patch -p1) < $f 
+		(cd gc; patch -p1) < $f 
 	fi
 done
-echo -n > ${GC}/gc_cpp.cc
+echo -n > gc/gc_cpp.cc
--- windows/makebin32.bat
+++ windows/makebin32.bat
@@ -1,16 +1,12 @@
-@rem $OpenXM: OpenXM_contrib2/windows/makebin32.bat,v 1.11 2013/08/30 19:09:28 ohara Exp $
+@rem $OpenXM: OpenXM_contrib2/windows/makebin32.bat,v 1.12 2014/12/31 19:57:00 ohara Exp $
 @echo off
 
 set ARCH=x86
 set INCLUDE=%INCLUDE%;%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include
 
-if not exist gc-7.2 ( c:\cygwin\bin\bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
-pushd gc-7.2
+if not exist gc ( c:\cygwin\bin\bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
+pushd gc
 nmake -f NT_STATIC_THREADS_MAKEFILE gc.lib
-popd
-
-pushd pari20
-nmake -f Makefile.vc6
 popd
 
 for %%i in ( asir2000lib engine2000 mcpp asir32gui post-msg-asirgui ..\asir2000 ) do (
