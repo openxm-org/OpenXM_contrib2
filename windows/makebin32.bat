0908206bb242c32b8ad1c8783b8ee3ba584473b1
ohara <>
2016-06-29 13:27:12 +0000

    Build system can use cygwin64

--- windows/makebin32.bat
+++ windows/makebin32.bat
@@ -1,10 +1,14 @@
-@rem $OpenXM: OpenXM_contrib2/windows/makebin32.bat,v 1.12 2014/12/31 19:57:00 ohara Exp $
+@rem $OpenXM: OpenXM_contrib2/windows/makebin32.bat,v 1.13 2015/08/13 13:45:26 ohara Exp $
 @echo off
 
 set ARCH=x86
 set INCLUDE=%INCLUDE%;%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include
 
-if not exist gc ( c:\cygwin\bin\bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
+setlocal
+set PATH=%PATH%;c:\cygwin\bin;c:\cygwin64\bin
+if not exist gc ( bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
+endlocal
+
 pushd gc
 nmake -f NT_STATIC_THREADS_MAKEFILE gc.lib
 popd
--- windows/makebin64.bat
+++ windows/makebin64.bat
@@ -1,10 +1,14 @@
-@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.14 2014/12/31 19:57:00 ohara Exp $
+@rem $OpenXM: OpenXM_contrib2/windows/makebin64.bat,v 1.15 2015/08/13 13:45:26 ohara Exp $
 @echo off
 
 set ARCH=AMD64
 set INCLUDE=%INCLUDE%;%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include
 
-if not exist gc ( c:\cygwin\bin\bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
+setlocal
+set PATH=%PATH%;c:\cygwin\bin;c:\cygwin64\bin
+if not exist gc ( bash -c 'PATH=/bin:$PATH ./extract_gc.sh' )
+endlocal
+
 pushd gc
 nmake -f NT_X64_STATIC_THREADS_MAKEFILE gc.lib
 popd
