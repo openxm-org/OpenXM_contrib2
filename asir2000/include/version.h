31c69a70eaeeba6c6708b38bfc840e0451badfba
ohara <>
2016-06-29 16:28:38 +0000

    Version is bumped.

--- asir2000/include/version.h
+++ asir2000/include/version.h
@@ -45,7 +45,7 @@
  * DEVELOPER SHALL HAVE NO LIABILITY IN CONNECTION WITH THE USE,
  * PERFORMANCE OR NON-PERFORMANCE OF THE SOFTWARE.
  *
- * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.387 2016/04/01 03:11:37 ohara Exp $ 
+ * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.388 2016/04/05 04:21:19 noro Exp $ 
 */
-#define ASIR_VERSION 20160405
+#define ASIR_VERSION 20160630
 #define ASIR_DISTRIBUTION "Kobe"
