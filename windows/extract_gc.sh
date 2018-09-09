dff6f85522fe6376139e7a5dc9715a1828b54cc8
ohara <>
2018-09-09 07:39:14 +0000

    GC is up to date.

--- windows/extract_gc.sh
+++ windows/extract_gc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# $OpenXM: OpenXM_contrib2/windows/extract_gc.sh,v 1.7 2013/08/20 14:17:22 ohara Exp $
+# $OpenXM: OpenXM_contrib2/windows/extract_gc.sh,v 1.8 2015/08/13 13:45:26 ohara Exp $
 (set -o igncr) 2>/dev/null && set -o igncr;#
 OpenXM_HOME=${OpenXM_HOME:-../../OpenXM}
 OpenXM_dist="$(realpath -m ${OpenXM_HOME}/../OpenXM_dist)"
-GC=gc-7.4.2
-ATOMIC_OPS=libatomic_ops-7.4.0
-GC_TARGZ=gc-7.4.2.tar.gz
+GC=gc-7.4.4
+ATOMIC_OPS=libatomic_ops-7.4.4
+GC_TARGZ=${GC}.tar.gz
 GC_PATCH=gc-7.0-risa.diff
 
 tar zxf ${OpenXM_dist}/${GC_TARGZ}
