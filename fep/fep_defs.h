3eca6b3d55c4f32dc6c512ae1054ad1cd84e9504
noro <>
2021-12-25 08:25:56 +0000

    Added support for M1.

--- fep/fep_defs.h
+++ fep/fep_defs.h
@@ -40,7 +40,7 @@
 #define toctrl(c)	((c)&~0100)
 #define ctrl(c)		((c)&037)
 
-#if defined(__alpha) || defined(__x86_64__) || defined(__amd64__) || defined(ANDROID)
+#if defined(__alpha) || defined(__x86_64__) || defined(__amd64__) || defined(ANDROID) || defined(__aarch64__)
 #define INDIRECTED      (((u_long)1)<<63)
 #define isIndirect(f)   ((u_long)(f)&(u_long)INDIRECTED)
 #define setIndirect(f)  (FUNC)((u_long)(f)|(u_long)INDIRECTED)
