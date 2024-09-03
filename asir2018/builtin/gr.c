1cf8a4dbaeb2ad3ce358c94277a25a6135ddc536
noro_masayuki <noro@dell-i7>
2024-09-04 06:46:51 +0900

    Removed some massages.

--- asir2018/builtin/gr.c
+++ asir2018/builtin/gr.c
@@ -2881,7 +2881,7 @@ LIST dp_symb_preproc_marked(NODE f,NODE g,NODE h)
     rarray[i].f = (DP)BDY(t); rarray[i].h = (DP)BDY(th);
     rarray[i].contain = (char *)CALLOC(nred,sizeof(char));
   }
-  get_eg(&t1); fprintf(asir_out,"symb step0"); print_split_eg(&t0,&t1); fprintf(asir_out," "); t0 = t1;
+//  get_eg(&t1); fprintf(asir_out,"symb step0"); print_split_eg(&t0,&t1); fprintf(asir_out," "); t0 = t1;
   for ( i = 0; i < nred; i++ ) {
     for ( j = 0; j < nred; j++ ) {
       dl = BDY(rarray[j].h)->dl;
@@ -2889,7 +2889,7 @@ LIST dp_symb_preproc_marked(NODE f,NODE g,NODE h)
         if ( dl_equal(nv,dl,m->dl) ) rarray[i].contain[j] = 1;
     } 
   }
-  get_eg(&t1); fprintf(asir_out,"step1"); print_split_eg(&t0,&t1); fprintf(asir_out," "); t0 = t1;
+//  get_eg(&t1); fprintf(asir_out,"step1"); print_split_eg(&t0,&t1); fprintf(asir_out," "); t0 = t1;
   for ( i = 0; i < nred; i++ ) {
     struct oMDP tmp;
     int k;
@@ -2907,7 +2907,7 @@ LIST dp_symb_preproc_marked(NODE f,NODE g,NODE h)
       rarray[k].contain[j] = rarray[k].contain[i];
     }
   }
-  get_eg(&t1); fprintf(asir_out,"step2"); print_split_eg(&t0,&t1); fprintf(asir_out,"\n"); t0 = t1;
+//  get_eg(&t1); fprintf(asir_out,"step2"); print_split_eg(&t0,&t1); fprintf(asir_out,"\n"); t0 = t1;
   for ( i = 0, t = red0; t; t = NEXT(t), i++ ) BDY(t) = (pointer)rarray[i].f;
   for ( i = 0, t = h0; t; t = NEXT(t), i++ ) BDY(t) = (pointer)rarray[i].h;
   MKLIST(l0,red0);
